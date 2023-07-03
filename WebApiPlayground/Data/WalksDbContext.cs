5390b5ab7d54902e16db150c2ded6636e010ebfe
Oliwia <liwiamilka@gmail.com>
2023-07-03 12:47:27 +0200

    Configures how migrations will work.

--- WebApiPlayground/Data/WalksDbContext.cs
+++ WebApiPlayground/Data/WalksDbContext.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using WebApiPlayground.Models.Domain;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace WebApiPlayground.Data
 {
@@ -15,7 +17,15 @@ namespace WebApiPlayground.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("WalksConnectionString"));
+
+            var connectionString = _configuration.GetConnectionString("WalksConnectionString");
+
+            /*By providing the assembly that contains your migrations, Entity Framework Core can locate and apply any pending migrations 
+            to the database during application startup.This ensures that the database schema is in sync with your code's model.*/
+            optionsBuilder.UseSqlServer(connectionString, options =>
+            {
+                options.MigrationsAssembly(typeof(WalksDbContext).Assembly.FullName);
+            });
         }
 
         public DbSet<Difficulty> Difficulties { get; set; }
