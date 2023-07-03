b17827d4159c39421f29dd4b217f3a18a04fa4e0
Oliwia <liwiamilka@gmail.com>
2023-07-03 11:35:19 +0200

    Creates example controller.

---
+++ WebApiPlayground/Controllers/StudentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiPlayground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] { "Jacek", "Kuba", "Oliwia" };
+            return Ok(students);
+        }
+    }
+}
