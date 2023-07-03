7cfc4972d84cfb0fd30f2c298a4e0a477a5d5fa7
Oliwia <liwiamilka@gmail.com>
2023-07-03 12:20:37 +0200

    Creates DBContext.

---
+++ WebApiPlayground/Data/WalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPlayground.Models.Domain;
+
+namespace WebApiPlayground.Data
+{
+    public class WalksDbContext: DbContext
+    {
+        public WalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
