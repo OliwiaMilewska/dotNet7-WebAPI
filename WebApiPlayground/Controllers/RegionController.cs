f367e01b9fbb5669d3a797a15715894874942762
Oliwia <liwiamilka@gmail.com>
2023-07-04 10:46:57 +0200

    Use DTOs not domain models.

--- WebApiPlayground/Controllers/RegionController.cs
+++ WebApiPlayground/Controllers/RegionController.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebApiPlayground.Data;
-using WebApiPlayground.Models.Domain;
+using WebApiPlayground.Models.DTOs;
 
 namespace WebApiPlayground.Controllers
 {
@@ -18,17 +17,41 @@ namespace WebApiPlayground.Controllers
         [HttpGet("GetAllRegions")]
         public IActionResult GetAllRegions()
         {
-            var regions = _dbContext.Regions.ToList();
-            return Ok(regions);
+            // Get Data from Database
+            var regionsDomain = _dbContext.Regions.ToList();
+
+            //Map Domain Models to DTOs
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regionsDomain)
+            {
+                regionsDto.Add(new RegionDto
+                {
+                    Id = region.Id,
+                    Name = region.Name,
+                    Code = region.Code,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+
+            // Return DTOs
+            return Ok(regionsDto);
         }
 
         [HttpGet("GetRegionById/{id}")]
         public IActionResult GetRegionById(Guid id)
         {
-            var region = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
-            if (region == null)
+            var regionDomain = _dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomain == null)
                 return NotFound();
-            return Ok(region);
+
+            var regionDto = new RegionDto
+            {
+                Id = id,
+                Name = regionDomain.Name,
+                Code = regionDomain.Code,
+                RegionImageUrl = regionDomain.RegionImageUrl
+            };
+            return Ok(regionDto);
         }
     }
 }
