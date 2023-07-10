2c9fd5edaee2a0d1029df8e388ee0b4f826af2e8
Oliwia <liwiamilka@gmail.com>
2023-07-10 15:12:56 +0200

    Prepares set up for JWT auth.

--- WebApiPlayground/Controllers/RegionController.cs
+++ WebApiPlayground/Controllers/RegionController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApiPlayground.CustomActionFilters;
 using WebApiPlayground.Data;
@@ -10,6 +11,7 @@ namespace WebApiPlayground.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegionController : ControllerBase
     {
         private readonly WalksDbContext _dbContext;
--- WebApiPlayground/Program.cs
+++ WebApiPlayground/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebApiPlayground.Data;
 using WebApiPlayground.Mappings;
 using WebApiPlayground.Repositories;
@@ -21,6 +24,22 @@ builder.Services.AddScoped<IWalkRepository, SqlWalkRepository>();
 // Add Auto Mapper
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
+// Add Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["JWT:Issuer"],
+        ValidAudience = builder.Configuration["JWT:Audiance"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +51,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
