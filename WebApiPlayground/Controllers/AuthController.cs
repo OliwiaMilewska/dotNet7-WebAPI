d5022bcc257cb5df8a91bbf97e4f5b625b2ee124
Oliwia <liwiamilka@gmail.com>
2023-07-11 18:23:06 +0200

    Login functionality.

---
+++ WebApiPlayground/Controllers/AuthController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPlayground.Models.DTOs;
+using WebApiPlayground.Repositories;
+
+namespace WebApiPlayground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public readonly UserManager<IdentityUser> _userManager;
+        private readonly ITokenRepository _tokenRepository;
+
+        public AuthController(UserManager<IdentityUser> userManager, ITokenRepository tokenRepository)
+        {
+            _userManager = userManager;
+            _tokenRepository = tokenRepository;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var user = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await _userManager.CreateAsync(user, registerRequestDto.Password);
+
+            if (identityResult.Succeeded)
+            {
+                if (registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                    identityResult = await _userManager.AddToRolesAsync(user, registerRequestDto.Roles);
+
+                if (identityResult.Succeeded)
+                    return Ok("User was registered. Please Login.");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginRequestDto.Username);
+
+            if (user != null)
+            {
+                var checkPassword = await _userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+                if (checkPassword)
+                {
+                    var roles = await _userManager.GetRolesAsync(user);
+                    if (roles != null)
+                    {
+                        var token = _tokenRepository.CreateJWTToken(user, roles.ToArray());
+                        var response = new LoginResponseDto
+                        {
+                            JwtToken = token,
+                        };
+                        return Ok(response);
+                    }
+                    else return BadRequest("No roles found");
+                }
+                else return BadRequest("Password was incorrect");
+            }
+            else return BadRequest("Username was incorrect");
+        }
+    }
+}
---
+++ WebApiPlayground/Models/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiPlayground.Models.DTOs
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        public string[] Roles { get; set; }
+    }
+}
