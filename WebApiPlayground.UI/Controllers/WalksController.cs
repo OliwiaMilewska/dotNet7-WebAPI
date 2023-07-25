94ae194e27039fb4e2e6c4dde6fe37f3f6eb676c
Oliwia <liwiamilka@gmail.com>
2023-07-25 09:48:50 +0200

    Creates page with walks.

---
+++ WebApiPlayground.UI/Controllers/WalksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using WebApiPlayground.UI.Models.DTO;
+
+namespace WebApiPlayground.UI.Controllers
+{
+    public class WalksController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public WalksController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = new List<WalkDto>();
+
+            try
+            {
+                var client = _httpClientFactory.CreateClient();
+                var httpResponseMesage = await client.GetAsync("https://localhost:7015/api/v1/Walks?pageNumber=1&pageSize=1000");
+                httpResponseMesage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMesage.Content.ReadFromJsonAsync<IEnumerable<WalkDto>>());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return View(response);
+        }
+    }
+}
---
+++ WebApiPlayground.UI/Models/DTO/WalkDto.cs
@@ -0,0 +1,16 @@
+﻿using WebApiPlayground.UI.Models.Domain;
+
+namespace WebApiPlayground.UI.Models.DTO
+{
+    public class WalkDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        public RegionDto Region { get; set; }
+        public Difficulty Difficulty { get; set; }
+    }
+}
